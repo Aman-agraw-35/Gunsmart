32b6ce74077ea871d4929022d3661a277a0db7cc
Aman Agrawal <aman.agraw.35@gmail.com>
2024-07-05 15:26:56 +0530

    some minor fixes

--- src/app/checkout/checkoutpage.tsx
+++ src/app/checkout/checkoutpage.tsx
@@ -62,7 +62,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
     return (
       <div className="flex items-center justify-center ">
         <div
-          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current bg-[#b585d] border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current bg-[#b5865d] border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
           role="status"
         >
           <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap bg-[#b5865d] !border-0 !p-0 ![clip:rect(0,0,0,0)]">
